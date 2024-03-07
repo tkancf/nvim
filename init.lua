86ab5af3fc1aaf32a93503ac6dc749e249305a62
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-07 11:05:57 +0900

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,38 @@
+-- Basic
+-- encoding
+vim.o.encofing = 'utf-8'
+vim.scriptencoding = 'utf-8'
+vim.o.ambiwidth = 'double'
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.visualbell = true
+vim.o.showmatch = true
+vim.o.matchtime = 1
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+vim.opt.clipboard:append{'unnamedplus'}
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath('cache') .. '/undo'
+
+-- remap
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", {noremap = true})
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.api.nvim_set_keymap('n', 'j', 'gj', {noremap = true})
+vim.api.nvim_set_keymap('n', 'k', 'gk', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gj', 'j', {noremap = true})
+vim.api.nvim_set_keymap('n', 'gk', 'k', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Enter><Enter>', ':<C-u>w<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohl<CR>', { noremap = true, silent = true})
+
+-- Plugin
+-- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,11 +46,3 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Example using a list of specs with the default options
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-
-require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-})
