d85de1a4cc589c7a71baabe8d7bae7c7647ec772
tkancf <18424095+tkancf@users.noreply.github.com>
2024-04-20 11:37:56 +0900

    コードの変更を記述するための折りたたみレベルを追加***

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ vim.o.hlsearch = true
 vim.opt.clipboard:append { 'unnamedplus' }
 vim.o.undofile = true
 vim.o.undodir = vim.fn.stdpath('cache') .. '/undo'
+vim.o.foldlevel = 1
 
 -- disable netrw
 vim.g.loaded_netrw = 1
@@ -55,7 +56,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     -- code highlight
     {
-        "nvim-treesitter/nvim-treesitter",
+        'nvim-treesitter/nvim-treesitter',
         build = ":TSUpdate"
     },
     -- colorscheme
@@ -172,5 +173,41 @@ require("lazy").setup({
         config = function()
             require('pretty-fold').setup()
         end
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+        -- event = {
+        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+        --   "BufReadPre path/to/my-vault/**.md",
+        --   "BufNewFile path/to/my-vault/**.md",
+        -- },
+        dependencies = {
+            -- Required.
+            'nvim-lua/plenary.nvim',
+            'hrsh7th/nvim-cmp',
+            'nvim-telescope/telescope.nvim',
+            'nvim-treesitter/nvim-treesitter',
+            -- see below for full list of optional dependencies 👇
+        },
+        opts = {
+            -- Base dir of Obsidian vault
+            workspaces = {
+                {
+                    name = "tkm",
+                    path = "~/Dropbox/tkm",
+                },
+            },
+            completion = {
+                -- Set to false to disable completion.
+                nvim_cmp = true,
+                -- Trigger completion at 2 chars.
+                min_chars = 2,
+            },
+        },
     },
 })
