7818c6817af029a87932d6021ffea636033fa7e4
tkancf <18424095+tkancf@users.noreply.github.com>
2024-05-01 16:16:04 +0900

    add newline markdown option

--- init.lua
+++ init.lua
@@ -197,6 +197,7 @@ require("lazy").setup({
             vim.g.vim_markdown_folding_level = 2
             vim.g.vim_markdown_frontmatter = 1
             vim.o.foldenable = false
+            vim.g.vim_markdown_new_list_item_indent = 0
         end
     },
     {
