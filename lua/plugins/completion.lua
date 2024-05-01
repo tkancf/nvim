535693afe6b75dfa35fedcefb3950a0c795ea3b8
tkancf <18424095+tkancf@users.noreply.github.com>
2024-05-01 17:40:38 +0900

    Delete unused comment and code

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,15 +3,12 @@ return {
     config = function()
         local cmp = require 'cmp'
         cmp.setup {
-            sources = {
-                { name = "neorg" },
-            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
             }),
         }
     end
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        -- Tree-sitterの設定
         require 'nvim-treesitter.configs'.setup {
             highlight = {
                 ensure_installed = { "markdown" },
-                enable = true, -- Tree-sitterハイライトを有効化
+                enable = true,
                 additional_vim_regex_highlighting = false,
             },
         }
